723be0192c9453c714b5b2eda670408b5a3e2201
Abdelrahman Sorour <abdosorour10@gmail.com>
2025-02-04 22:26:02 +0200

    Add files via upload

---
+++ EF01-Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using EF1_Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+// DESKTOP-K2FJ8AV
+namespace EF1_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //using CompanyDbContext dbContext = new CompanyDbContext();
+            //var employee = dbContext.Employees.Where(E => E.Age > 10);
+            //dbContext.Database.Migrate();
+            //CompanyDbContext dbContext = new CompanyDbContext();
+            //try
+            //{
+            //}
+            //finally
+            //{
+            //    dbContext.Dispose();
+            //}
+        }
+
+    }
+}
