2972ce8f75ba033c85a64697337da8f991f7a95f
Abdelrahman Sorour <abdosorour10@gmail.com>
2025-02-04 22:27:24 +0200

    Add files via upload

---
+++ EF01-Demo/Data/Models/CompanyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF1_Demo.Data.Models
+{
+    internal class CompanyDbContext : DbContext
+    {
+        //CompanyDbContext() : base() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-K2FJ8AV; Database = CompanyG03; Trusted_Connection = true; trustservercertificate = true");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EF01-Demo/Data/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF1_Demo.Data.Models
+{
+    // POCO Class => Plain old Clr object
+    // We have 4 ways to map classes into tables
+    // 1- By convention [Default behavoiur]
+    // 2- 
+    internal class Employee
+    {
+        public int Id { get; set; } // Oublic numeric property named as "Id"
+        public string? EmployeeName { get; set; } // nvarchar(max)
+        // .net 5 : reference type : allow null [Optional]
+        // .net 6 : reference type : not allow null [Required]
+        public decimal Salary { get; set; } // decimal(18,2)
+        public int? Age { get; set; } // Nullable<int>
+    }
+}
